bd7f5e13cfa5645f3ededb92b64e5d072d936e62
Irina-Zhuk <irinamzh@gmail.com>
2024-07-30 00:49:27 -0400

    refactor typeDefs

--- index.js
+++ index.js
@@ -1,35 +1,13 @@
-// const  {ApolloServer} = require('apollo-server');
-// const mongoose = require('mongoose');
-// const typeDefs = require('./graphql/typeDefs');
-// const resolvers = require('./qraphql/resolvers');
-//
-// const MONGODB = "mongodb+srv://irinamzh:6NwZgog4mlBew2mc@cluster0.lvzg3fs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-//
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers
-// })
-//
-// mongoose.connect(MONGODB, {useNewUrlParser: true})
-//     .then(() => {
-//       console.log("MongoDB Connection successful");
-//       return server.listen({port: 5000})
-//     })
-//     .then((res) => {
-//       console.log(`Server running at ${res.url}`);
-//     })
-
-
 const { ApolloServer } = require('apollo-server');
 const mongoose= require('mongoose')
 const typeDefs = require('./graphql/typeDefs')
 const resolvers = require('./graphql/resolvers')
 
-const MONGODB = 'mongodb+srv://irinamzh:6NwZgog4mlBew2mc@cluster0.lvzg3fs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const MONGODB = "mongodb+srv://irinamzh:6NwZgog4mlBew2mc@cluster0.lvzg3fs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const server =new ApolloServer( {
     typeDefs,
     resolvers
-})
+});
 
 const connectDB = async () => {
     try {
