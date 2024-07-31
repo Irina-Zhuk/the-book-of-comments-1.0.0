88b51788987df61ef0d1cef2f57679a9c2cadef6
Irina-Zhuk <irinamzh@gmail.com>
2024-07-30 20:02:32 -0400

    set up completed

--- models/Comment.js
+++ models/Comment.js
@@ -1,57 +1,25 @@
-const mongoose = require('mongoose')
-const {model, Schema, Types} = require('mongoose')
+const { model, Schema} = require('mongoose')
+const mongoose = require("mongoose");
 mongoose.Schema.Types.String.set('trim', true);
 
-const commentSchema = new Schema({
+const commentSchema =
+
+    new Schema({
         _id: mongoose.Schema.Types.ObjectId,
-        user: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "User"
-            },
-        ],
-        createdAt: {
-            type: String,
-        },
-        rating: {
-            type: String,
-        },
-        rating: {
-            type: Number,
-        },
-        title: {
-            type: String,
-        },
-        description: {
-            type: String,
-        }
-    },
-)
-module.exports = model("Comments", commentSchema)
 
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
 
-// const { model, Schema} = require('mongoose')
-// const mongoose = require("mongoose");
-// mongoose.Schema.Types.String.set('trim', true);
-//
-// const commentSchema =
-//
-//     new Schema({
-//         _id: mongoose.Schema.Types.ObjectId,
-//
-//         user: {
-//             type: mongoose.Schema.Types.ObjectId,
-//             ref: 'User'
-//         },
-//
-//         createdAt: String,
-//
-//         rating: Number,
-//
-//         title: String,
-//
-//         description: String,
-//
-//     });
-//
-// module.exports = model('Comment', commentSchema)
+        createdAt: String,
+
+        rating: Number,
+
+        title: String,
+
+        description: String,
+
+    });
+
+module.exports = model('Comment', commentSchema)
