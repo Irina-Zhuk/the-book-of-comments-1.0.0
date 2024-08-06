f950e72ce9dd79266be03cbc76718a993393f29b
Irina-Zhuk <irinamzh@gmail.com>
2024-08-05 23:18:53 -0400

    UserCreate started

---
+++ src/_test/userCreate.spec.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+const {expect} = require('chai');
+
+describe('USER CREATE', () => {
+    describe('USER CREATE - POSITIVE TESTS', () => {
+        it('Should Create User with All Fields', () => {
+
+        })
+    })
+    describe('USER CREATE - NEGATIVE TESTS', () => {
+        it('Should not Create User... ', () => {
+
+        })
+    })
+})
